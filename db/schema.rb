6ff00033f2c14a2712a4016101d898fb4a4396de
jcmunav63 <jcmunav63@gmail.com>
2024-02-19 22:40:17 -0600

    Create models for Category, Expense, and CategoryExpense (join table). Migrate the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_19_223130) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_043111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.decimal "total_expenses"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_expenses", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "expense_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_expenses_on_category_id"
+    t.index ["expense_id"], name: "index_category_expenses_on_expense_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +55,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_19_223130) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "category_expenses", "categories"
+  add_foreign_key "category_expenses", "expenses"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
---
+++ spec/factories/category_expenses.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category_expense do
+    category { nil }
+    expense { nil }
+  end
+end
