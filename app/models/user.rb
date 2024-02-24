460b425b3308cf5818fabed551c3403194077c76
jcmunav63 <jcmunav63@gmail.com>
2024-02-23 20:38:05 -0600

    Set up CanCanCan, add ability file, add role to database, add methods and roles to User model, and update views (categories & expenses).

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   has_many :categories, foreign_key: :user_id, class_name: 'Category'
   has_many :expenses, foreign_key: :author_id, class_name: 'Expense'
+
+  # User::Roles
+  enum role: %i[default admin].freeze
+
+  def admin?
+    role == 'admin'
+  end
+
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
 end
