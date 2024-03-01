bb481a09631dfbd829d4d5903d19c5eae9c0c3bf
jcmunav63 <jcmunav63@gmail.com>
2024-03-01 17:04:16 -0600

    Changed the migration file (users column).

--- db/migrate/20240224012811_add_role_to_users.rb
+++ db/migrate/20240224012811_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :role, :string, :default: 'default' # Update this line
+    add_column :users, :role, :string, default: 'default'
   end
 end
