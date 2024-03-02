52a46585987847e8e75a39b668ad135307d1cb15
jcmunav63 <jcmunav63@gmail.com>
2024-03-02 00:42:50 -0600

    Edit new expense page & expenses controller.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -22,26 +22,75 @@ class ExpensesController < ApplicationController
 
   def create
     @category = Category.find(params[:category_id])
-    @expense = @category.expenses.build(expense_params)
-    @expense.author_id = current_user.id
-    if @expense.save
-      # Create a corresponding CategoryExpense record
-      @category_expense = @category.category_expenses.create(expense_id: @expense.id)
-      # Update the total_expenses for the associated category
-      @category.update(total_expenses: @category.total_expenses + @expense.amount)
-      flash[:notice] = 'Expense was successfully created.'
-      redirect_to user_category_expenses_path(current_user, @category)
+    @categories = current_user.categories.where.not(id: @category.id)
+    @amount = params[:amount]
+    @name = params[:name]
+    selected_category_ids = params[:expense][:categories].reject(&:empty?).map(&:to_i)
+    if selected_category_ids.empty?
+      @expense = @category.expenses.build(expense_params)
+      @expense.author_id = current_user.id
+      if @expense.save
+        # Create a corresponding CategoryExpense record
+        @category_expense = @category.category_expenses.create(expense_id: @expense.id)
+        # Update the total_expenses for the associated category
+        @category.update(total_expenses: @category.total_expenses + @expense.amount)
+        redirect_to user_category_expenses_path(current_user, @category), notice: 'Expense was successfully created.'
+      else
+        render :new
+      end
     else
-      render :new
+      save_for_two_categories(@category, selected_category_ids, @amount, @name)
+      redirect_to user_category_expenses_path(current_user, @category), notice: 'Creating a Expense for two categories.'
     end
   rescue StandardError => e
     flash[:alert] = "Error creating expenses: #{e.message}"
     render :new
   end
 
+  def save_for_two_categories(category, selected_category_ids, amount, name)
+    @total_categories = selected_category_ids.length + 1
+    if @total_categories == 2
+      @expense = category.expenses.build(expense_params)
+      @expense.author_id = current_user.id
+      if @expense.save
+        @half = @expense.amount/2
+        @expense.update(amount: @expense.amount - @half)
+        # Create a corresponding CategoryExpense record
+        @category_expense = category.category_expenses.create(expense_id: @expense.id)
+        # Update the total_expenses for the associated category
+        category.update(total_expenses:category.total_expenses + @half)
+
+        # Create second Expense (second category with half of amountl)
+        @category2 = Category.find(selected_category_ids[1])
+        puts "Category 2: #{@category2.inspect}"
+        @expense2 = Expense.new
+        @expense2 = @category2.expenses.build(expense_params2) # CHECK OTHER 
+        @expense2.author_id = current_user.id
+        if @expense2.save
+          @expense2.update(amount: @expense2.amount - @half)
+          puts "Expense (half): #{@expense2.inspect}"
+          # Create a corresponding CategoryExpense record
+          @category_expense2 = @category2.category_expenses.create(expense_id: @expense2.id)
+          puts @category_expense2.inspect
+          # Update the total_expenses for the associated category
+          @category2.update(total_expenses: @category2.total_expenses + @half) # CHECK
+          puts "Category with half updated: #{@category2.inspect}"
+        end
+        redirect_to user_category_expenses_path(current_user, category), notice: 'Expense was successfully created.'
+      else
+        puts "Rendering again the EXPENSES_NEW page"
+        render :new
+      end
+    end
+  end
+
   private
 
   def expense_params
+    params.require(:expense).permit(:name, :amount, :categories)
+  end
+
+  def expense_params2
     params.require(:expense).permit(:name, :amount)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_012811) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_233829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
